4f9667287d860dc586a7110468ec70f57845d76b
me help me <nirarazi14@gmail.com>
2025-05-20 05:39:38 +0300

    Create inetsim_installer.sh

---
+++ inetsim_installer.sh
@@ -0,0 +1,40 @@
+#!/bin/bash 
+root=$(whoami)
+
+if [[ "$root" == "root" ]] ; then
+if command -v inetsim  > /dev/null ; then
+echo "[+] inetsim is installed"
+echo
+else
+apt-get install inetsim -y > /dev/null 2>&1
+echo "[+] Installation complete."
+echo
+fi
+if command -v cpanminus > /dev/null ; then
+echo "[+] cpanminus is installed"
+echo
+else
+apt-get install cpanminus -y > /dev/null 2>&1
+echo "[+] Installation complete."
+fi
+echo
+echo "[-] Downgrading INetSim"
+echo
+cpanm --install Net::DNS@1.22  > /dev/null 2>&1
+echo "[+] Downgrading complete"
+echo
+sed -i 's/^service_bind_address.*/service_bind_address 0.0.0.0/' /etc/inetsim/inetsim.conf > /dev/null 2>&1
+echo "[!] Restarting INetSim"
+service inetsim restart 
+check=$(netstat -tupna | grep 53  | grep -i inetsim)
+if [[ -z  $check ]] ; then
+echo
+echo "[-] INetSim isn't running. Please check your system configuration."
+else
+echo
+echo "[+] INetSim is running — you're good to go!"
+fi
+else
+echo "[-] Please run the script as root."
+exit 1
+fi
